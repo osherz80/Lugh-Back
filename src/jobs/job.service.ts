87d0d8ec19796bcfd07645eeb40f1c3e4327054f
Osher <osherzohar80@gmail.com>
2026-04-14 22:35:11 +0300

    working search

--- src/jobs/job.service.ts
+++ src/jobs/job.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { desc, sql } from 'drizzle-orm';
 import { getEmbedding } from 'src/common/helpers/ai';
 import { db } from 'src/db';
 import { jobs } from 'src/db/schema';
@@ -19,6 +20,34 @@ export class JobsService {
         embedding: embedding,
       });
       return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async searchJobs(jobSearch: string) {
+    if (!jobSearch) {
+      throw new Error('Search job is required');
+    }
+    try {
+      let embeddedQuery = await getEmbedding(jobSearch);
+      embeddedQuery = embeddedQuery.slice(0, 256);
+
+      const similarity = sql<number>`1 - (${jobs.embedding} <=> ${JSON.stringify(embeddedQuery)})`;
+
+      const results = await db
+        .select({
+          id: jobs.id,
+          title: jobs.title,
+          description: jobs.description,
+          score: similarity,
+        })
+        .from(jobs)
+        .where(sql`${similarity} > 0.1`)
+        .orderBy(t => desc(t.score))
+        .limit(10);
+
+      return results;
     } catch (err) {
       console.log(err);
     }
