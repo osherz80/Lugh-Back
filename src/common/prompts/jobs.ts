aae2b9ca4d2c94572b282991177948ccf2ac18ae
Osher <osherzohar80@gmail.com>
2026-09-09 15:13:40 +0300

    auto job workingish

---
+++ src/common/prompts/jobs.ts
@@ -0,0 +1,3 @@
+export const JOB_POST_EXTRACTOR = `extract the details from this job posting [JOB_POST], return a json with valid details
+that was mentioned in the job post explicitly only
+`
--- src/jobs/job.service.ts
+++ src/jobs/job.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { desc, sql } from 'drizzle-orm';
-import { getEmbedding } from 'src/common/helpers/ai';
+import { askAiV2, getEmbedding } from 'src/common/helpers/ai';
 import { db } from 'src/db';
 import { jobs } from 'src/db/schema';
 import { Job } from 'src/common/types/general';
+import { JOB_POST_EXTRACTOR } from 'src/common/prompts/jobs';
 
 @Injectable()
 export class JobsService {
@@ -25,15 +26,48 @@ export class JobsService {
     }
   }
 
-  async autoCreateJob(job: Job) {
+  async autoCreateJob(job: { description: string }) {
     if (!job) {
       throw new Error('Job is required');
     }
 
+    const jobSchema = {
+      type: 'OBJECT',
+      properties: {
+        title: { type: 'STRING' },
+        description: { type: 'STRING' },
+        responsibilities: { type: 'STRING' },
+        requirements: { type: 'STRING' },
+        niceToHave: { type: 'STRING' },
+        benefits: { type: 'STRING' },
+        aboutCompany: { type: 'STRING' },
+        department: { type: 'STRING' },
+        location: { type: 'STRING' },
+        workModel: { type: 'STRING' },
+        employmentType: { type: 'STRING' },
+        experienceLevel: { type: 'STRING' },
+      },
+      required: [
+        'title',
+        'description',
+        'responsibilities',
+        'requirements',
+        'niceToHave',
+        'benefits',
+        'aboutCompany',
+        'department',
+        'location',
+        'workModel',
+        'employmentType',
+        'experienceLevel',
+      ]
+    };
+
     try {
-      let embedding = await getEmbedding(job.description);
+      let embedding = await getEmbedding(`${job.description}`);
+      const structuredJob = await askAiV2<Job>(JOB_POST_EXTRACTOR, `here is the [JOB_POST]: ${job.description}`, 0.3, jobSchema)
       const result = await db.insert(jobs).values({
-        ...job,
+        ...structuredJob,
         embedding
       });
       return result;
